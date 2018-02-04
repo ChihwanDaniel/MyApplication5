7d22cf4cd6dbff33f5d92845df490179494ac5da
456161 <565628998419849>
2018-02-04 21:07:47 +0900

    암시적 파일 열기
    pdf파일 열기를 암시적인 형식만 지정해주고 열라고 시키는 거

--- app/src/main/java/com/example/prinston/myapplication/MainActivity.java
+++ app/src/main/java/com/example/prinston/myapplication/MainActivity.java
@@ -1,12 +1,17 @@
 package com.example.prinston.myapplication;
 
+import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.net.Uri;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
     EditText et;
@@ -17,17 +22,36 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         et = (EditText)findViewById(R.id.editText);
-
-        Button btn = (Button)findViewById(R.id.button);
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String data = et.getText().toString();
-
-                Intent intent = new Intent(Intent.ACTION_DIAL, Uri.parse(data));
-                startActivity(intent);
-            }
-        });
     }
 
+    public void onButton1Clicked(View view){
+        String filename = et.getText().toString();
+        if(filename.length() > 0){
+            openPDF(filename.trim());
+        }else{
+            Toast.makeText(getApplicationContext(),"얌마, PDF파일 이름을 입력하라고 짜샤",Toast.LENGTH_LONG).show();
+        }
+    }
+
+    public void openPDF(String filename){
+        String sdcardFolder = Environment.getExternalStorageDirectory().getAbsolutePath();
+        String filepath = sdcardFolder + File.separator + filename;
+        File file = new File(filepath);
+        Toast.makeText(this,sdcardFolder,Toast.LENGTH_LONG).show();
+
+        if(file.exists()){
+            Uri uri = Uri.fromFile(file);
+
+            Intent intent = new Intent(Intent.ACTION_VIEW);
+            intent.setDataAndType(uri,"application/pdf");
+
+            try{
+                startActivity(intent);
+            }catch(ActivityNotFoundException e){
+                Toast.makeText(this,"PDF 볼 뷰어가 없잖아!", Toast.LENGTH_SHORT).show();
+            }
+        }else{
+            Toast.makeText(this,"어이,PDF파일을 찾을 수 없거든? 나 놀리냐?",Toast.LENGTH_LONG).show();
+        }
+    }
 }
