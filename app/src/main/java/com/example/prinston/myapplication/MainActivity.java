ca829494f9dac90cda7c0df34fe5b1c3b9b3cfbe
456161 <565628998419849>
2018-02-04 19:57:56 +0900

    Intent사용 연습
    버튼을 클릭하면 EditText에 문자열로 다이얼 입력

--- app/src/main/java/com/example/prinston/myapplication/MainActivity.java
+++ app/src/main/java/com/example/prinston/myapplication/MainActivity.java
@@ -1,15 +1,33 @@
 package com.example.prinston.myapplication;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    EditText et;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        et = (EditText)findViewById(R.id.editText);
 
+        Button btn = (Button)findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String data = et.getText().toString();
+
+                Intent intent = new Intent(Intent.ACTION_DIAL, Uri.parse(data));
+                startActivity(intent);
+            }
+        });
     }
+
 }
